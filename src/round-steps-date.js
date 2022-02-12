55fbab401d0b027841ae76aec86de6802eed4cef
KM <the.real.laplace@gmail.com>
2022-02-12 04:04:30 +0200

    chore(commit): force

--- src/round-steps-date.js
+++ src/round-steps-date.js
@@ -1,8 +1,9 @@
-const { PARTS, getPart } = require('@kmamal/util/date/parts')
-const { elapsed } = require('@kmamal/util/date/elapsed')
 const { duration } = require('@kmamal/util/date/duration')
+const { PARTS, fromTimestamp } = require('@kmamal/util/date/date')
+const { elapsed } = require('@kmamal/util/date/elapsed')
 const { ceil, floor } = require('@kmamal/util/date/rounding')
 const { shift } = require('@kmamal/util/date/shift')
+const { clone } = require('@kmamal/util/date/clone')
 
 const factors10 = [ 10, 5, 2, 1 ]
 const factors12 = [ 6, 4, 3, 2, 1 ]
@@ -21,9 +22,9 @@ const partFactors = {
 }
 
 const roundStepsDate = (_start, _end, step) => {
-	const start = new Date(_start)
-	const end = new Date(_end)
-	if (start.getTime() === end.getTime()) { return null }
+	const start = fromTimestamp(_start)
+	const end = fromTimestamp(_end)
+	if (start.timestamp === end.timestamp) { return null }
 
 	const minStep = Math.abs(step)
 
@@ -35,7 +36,7 @@ const roundStepsDate = (_start, _end, step) => {
 		part = PARTS[index]
 		const e = elapsed(start, end, part)
 		if (e !== 0) { break }
-		constantParts[part] = getPart(start, part)
+		constantParts[part] = start[part]
 		index++
 	}
 
@@ -62,14 +63,18 @@ const roundStepsDate = (_start, _end, step) => {
 	const round$$$ = isPositive ? ceil.$$$ : floor.$$$
 
 	round$$$(start, stepPart)
-	let startPartDiff = -getPart(start, stepPart, true) % stepValue
+	let startPart = start[stepPart]
+	if (stepPart === 'month' || stepPart === 'day') { startPart-- }
+	let startPartDiff = -startPart % stepValue
 	if (startPartDiff !== 0 && isPositive) {
 		startPartDiff = stepValue + startPartDiff
 	}
 	shift.$$$(start, stepPart, startPartDiff)
 
 	round$$$(end, stepPart)
-	let endPartDiff = -getPart(end, stepPart, true) % stepValue
+	let endPart = end[stepPart]
+	if (stepPart === 'month' || stepPart === 'day') { endPart-- }
+	let endPartDiff = -endPart % stepValue
 	if (endPartDiff !== 0 && !isPositive) {
 		endPartDiff = stepValue + endPartDiff
 	}
@@ -87,9 +92,9 @@ const roundStepsDate = (_start, _end, step) => {
 }
 
 const iterate = function * (start, end, stepValue, stepPart) {
-	const endTime = end.getTime()
-	const x = new Date(start)
-	while (x.getTime() !== endTime) {
+	const endTime = end.timestamp
+	const x = clone(start)
+	while (x.timestamp !== endTime) {
 		yield x
 		shift.$$$(x, stepPart, stepValue)
 	}
